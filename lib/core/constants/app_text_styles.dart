799fc6c0c8464f49ae949abe048d46683b16aca1
@herlambangkurniawan <gilbertrafdi@gmail.com>
2025-05-15 22:32:11 +0700

    feat(core): define app colors and text styles

---
+++ lib/core/constants/app_text_styles.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle headingLarge = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    fontSize: 28.0,
+    height: 1.3,
+    color: Colors.black,
+  );
+
+  static const TextStyle headingMedium = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    fontSize: 21.0,
+    height: 1.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle subtitle = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    fontSize: 18.0,
+    height: 1.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle bodyLarge = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+    fontSize: 14.0,
+    height: 1.45,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    fontSize: 14.0,
+    height: 1.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle label = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    fontSize: 12.0,
+    height: 1.0,
+    letterSpacing: 0.05,
+    textBaseline: TextBaseline.alphabetic,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    fontSize: 11.0,
+    height: 1.0,
+    letterSpacing: 0.0,
+  );
+}
